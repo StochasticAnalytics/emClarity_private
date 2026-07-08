c946f2c505e714cc22ca28121883f4cca729d812
himesb <ben@clearnoise.org>
2026-07-08 11:06:31 -0400

    cleanup chore

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,18 +1,6 @@
 from backend.models.base import CamelCaseModel
 from backend.models.job import Job, JobStatus
 from backend.models.parameter import ParameterDefinition, ParameterFile, ParameterValue
-from backend.models.pipeline import (
-    CreatePipelineItemRequest,
-    PipelineItem,
-    PipelineItemEnriched,
-    PipelineLink,
-    PipelineOrigin,
-    PipelinePriority,
-    PipelineStage,
-    PlanSubStatus,
-    PrdTaskSummary,
-    UpdatePipelineItemRequest,
-)
 from backend.models.project import Project, ProjectState
 from backend.models.project_settings import (
     ProjectSettings,
@@ -24,25 +12,15 @@ from backend.models.workflow import CommandRequest, PipelineCommand
 __all__ = [
     "CamelCaseModel",
     "CommandRequest",
-    "CreatePipelineItemRequest",
     "Job",
     "JobStatus",
     "ParameterDefinition",
     "ParameterFile",
     "ParameterValue",
     "PipelineCommand",
-    "PipelineItem",
-    "PipelineItemEnriched",
-    "PipelineLink",
-    "PipelineOrigin",
-    "PipelinePriority",
-    "PipelineStage",
-    "PlanSubStatus",
-    "PrdTaskSummary",
     "Project",
     "ProjectSettings",
     "ProjectSettingsPatch",
     "ProjectState",
     "RunProfile",
-    "UpdatePipelineItemRequest",
 ]
